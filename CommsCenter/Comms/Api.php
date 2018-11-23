4210137671b5fcfc03a9e87d267116939ff8c596
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-11-23 18:11:45 +0100

    Comms API - initial commit

---
+++ CommsCenter/Comms/Api.php
@@ -0,0 +1,33 @@
+<?php namespace CommsCenter\Comms;
+
+use GuzzleHttp\RequestOptions;
+use Pckg\Api\Api as PckgApi;
+
+/**
+ * Class Api
+ *
+ * @package CommsCenter\Comms
+ */
+class Api extends PckgApi
+{
+
+    /**
+     * Api constructor.
+     *
+     * @param $endpoint
+     * @param $apiKey
+     */
+    public function __construct($endpoint, $apiKey)
+    {
+        $this->endpoint = $endpoint;
+        $this->apiKey = $apiKey;
+
+        $this->requestOptions = [
+            RequestOptions::HEADERS => [
+                'X-Comms-Api-Key' => $this->apiKey,
+            ],
+            RequestOptions::TIMEOUT => 15,
+        ];
+    }
+
+}
